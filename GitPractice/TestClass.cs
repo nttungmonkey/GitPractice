37fb749cac953f208ded03ac85fb6909f47e257b
withtalktest1 <withtalktest1@gmail.com>
2025-06-12 16:14:39 +0900

    add

---
+++ GitPractice/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitPractice
+{
+    internal class TestClass
+    {
+        int a = 10;
+    }
+}
